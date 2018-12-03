45bdb0fe4ed98a6499efdf70e9bb717b5f4a5a50
Matt Readout <matt.readout@gmail.com>
2018-12-03 15:09:24 -0500

    add users views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :favorites
   has_many :recipes, through: :favorites
+  validates :username, uniqueness: true
+  validates :first_name, :last_name, presence: true
 
   def full_name
     self.first_name + " " + self.last_name
