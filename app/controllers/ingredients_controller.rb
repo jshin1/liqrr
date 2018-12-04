89586269c9e261637365b59fa9154f2612e4154a
github username <github email address>
2018-12-04 09:43:54 -0500

    Create search function

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,47 @@
 class IngredientsController < ApplicationController
+
+  before_action :find_ingredient, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def show
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.create(ingredient_params)
+
+    if @ingredient.valid?
+      redirect_to @ingredient
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def
+
+  private
+
+  def find_recipe
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :alcoholic, :mixer, :description)
+  end
+
 end
