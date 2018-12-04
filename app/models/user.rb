ebb6080b66a80c616c14d05f2af6a93571ddd32b
Matt Readout <matt.readout@gmail.com>
2018-12-04 10:08:26 -0500

    add seed function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :favorites
   has_many :recipes, through: :favorites
+  validate :age
   validates :username, uniqueness: true
   validates :first_name, :last_name, presence: true
   validates_confirmation_of :password
@@ -12,5 +13,11 @@ class User < ApplicationRecord
   def age
     now = Time.now.utc.to_date
     now.year - self.birthdate.year - ((now.month > self.birthdate.month || (now.month == self.birthdate.month && now.day >= self.birthdate.day)) ? 0 : 1)
+  end
+
+  def over_21
+    if self.age < 21
+      errors.add(:birthdate, "Users must be 21 years of age or older to create an account")
+    end 
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,38 @@
+require 'rest-client'
+require 'json'
+
+API = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
+id_count =  0
+
+def request(url, id)
+  response_string = RestClient.get(url + id.to_s)
+  response_hash = JSON.parse(response_string)
+  response_hash
+end
+
+# request(API, '12402')['drinks'][0]
+# this gets us a hash of one drink
+
+# id_count = 0
+# while id_count < 20
+#   id_count += 1
+#   drink_hash = request(API, id_count)
+#     if drink_hash['drinks']
+#       recipe = Recipe.new
+# end
+
+
+
+def seed_db
+  drink_hash = request(API, '12402')['drinks'][0]
+  new_recipe = Recipe.create(name: drink_hash['strDrink'], instructions: drink_hash['strInstructions'], glass: drink_hash['strGlass'], img_url: drink_hash['strDrinkThumb'], category: drink_hash['strCategory'])
+  RecipeIngredient.create(recipe: new_recipe, measurement: drink_hash['strMeasure1'], ingredient: Ingredient.find_or_create_by(name: drink_hash['strIngredient1']))
+  RecipeIngredient.create(recipe: new_recipe, measurement: drink_hash['strMeasure2'], ingredient: Ingredient.find_or_create_by(name: drink_hash['strIngredient2']))
+end
+
+seed_db
+byebug
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
