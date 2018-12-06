62000ad8c0cf2c4d36a05fa712b45d75e5a61531
Matt Readout <matt.readout@gmail.com>
2018-12-06 13:46:12 -0500

    finalize db seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,18 +25,18 @@ require 'json'
 
 # def seed_db
 #   ingredient_count = 0
-#   drink_id = 1
-#   while drink_id < 17500
+#   drink_id = 18001
+#   while drink_id < 19000
 #     drink_id += 1
 #     drink_hash = request(API, drink_id.to_s)
 #     if drink_hash['drinks']
-#       new_recipe = Recipe.create(name: drink_hash['strDrink'], instructions: drink_hash['strInstructions'], glass: drink_hash['strGlass'], img_url: drink_hash['strDrinkThumb'], category: drink_hash['strCategory'])
+#       new_recipe = Recipe.create(name: drink_hash['drinks'][0]['strDrink'], instructions: drink_hash['drinks'][0]['strInstructions'], glass: drink_hash['drinks'][0]['strGlass'], img_url: drink_hash['drinks'][0]['strDrinkThumb'], category: drink_hash['drinks'][0]['strCategory'])
 #       while ingredient_count < 15
 #         ingredient_count += 1
 #         measure_arg = 'strMeasure' + ingredient_count.to_s
 #         ingredient_arg = 'strIngredient' + ingredient_count.to_s
-#         if drink_hash[measure_arg] != "" && drink_hash[measure_arg] != " "
-#           RecipeIngredient.create(recipe: new_recipe, measurement: drink_hash[measure_arg], ingredient: Ingredient.find_or_create_by(name: drink_hash[ingredient_arg]))
+#         if drink_hash['drinks'][0][measure_arg] != "" && drink_hash['drinks'][0][measure_arg] != " "
+#           RecipeIngredient.create(recipe: new_recipe, measurement: drink_hash['drinks'][0][measure_arg], ingredient: Ingredient.find_or_create_by(name: drink_hash['drinks'][0][ingredient_arg]))
 #         end
 #       end
 #      ingredient_count = 0
@@ -55,15 +55,15 @@ require 'json'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.delete_all
-Recipe.delete_all
-
-User.create(username: "mattyboi", first_name: "Matt", last_name: "Readout", password: "password123")
-
-Recipe.create(name: "Old Fashioned", occasion: "when you're feeling like a godfather", category: "Cocktail" , instructions: "N/A", glass: "Old fashioned glass", img_url: "//cdn.liquor.com/wp-content/uploads/2018/05/08113350/bourbon-old-fashioned-720x720-recipe.jpg")
-Recipe.create(name: "Cuba Libre", occasion: "when you're chilling in LES", category: "Highball" , instructions: "N/A", glass: "Highball glass")
-Recipe.create(name: "Dirty Martini", occasion: "when a regular martini ain't cutting it", category: "Martini", instructions: "wd;lnkwdlkfn", glass: "Martini glass")
-
-Ingredient.create(name: "Gin", ing_type: "Gin", description: "Gin")
-
-RecipeIngredient.create(measurement: "1 oz", recipe: Recipe.find_by(name: "Dirty Martini"), ingredient: Ingredient.find_by(name: "Gin"))
+# User.delete_all
+# Recipe.delete_all
+#
+# User.create(username: "mattyboi", first_name: "Matt", last_name: "Readout", password: "password123")
+#
+# Recipe.create(name: "Old Fashioned", occasion: "when you're feeling like a godfather", category: "Cocktail" , instructions: "N/A", glass: "Old fashioned glass", img_url: "//cdn.liquor.com/wp-content/uploads/2018/05/08113350/bourbon-old-fashioned-720x720-recipe.jpg")
+# Recipe.create(name: "Cuba Libre", occasion: "when you're chilling in LES", category: "Highball" , instructions: "N/A", glass: "Highball glass")
+# Recipe.create(name: "Dirty Martini", occasion: "when a regular martini ain't cutting it", category: "Martini", instructions: "wd;lnkwdlkfn", glass: "Martini glass")
+#
+# Ingredient.create(name: "Gin", ing_type: "Gin", description: "Gin")
+#
+# RecipeIngredient.create(measurement: "1 oz", recipe: Recipe.find_by(name: "Dirty Martini"), ingredient: Ingredient.find_by(name: "Gin"))
